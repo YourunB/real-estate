0ad52e0a2d3e1f6843b6f40ed457b3871ae1b21d
Yury Butskevich <bxyura@gmail.com>
2024-04-05 01:12:27 +0300

    feat: add overlay and modal window

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const menu = document.getElementById('menu-window');
 const btnMenu = document.getElementById('btn-burger-menu');
+const form = document.getElementById('form-calculate');
+const btnCalculate = document.getElementById('btn-form-calculate');
+const overlay = document.getElementById('overlay');
+const btnModal = document.getElementById('modal-btn');
 
 btnMenu.addEventListener('click', () => {
   menu.classList.toggle('menu_show');
   if (menu.classList.contains('menu_show')) btnMenu.classList.add('header__burger-menu_click');
   else btnMenu.classList.remove('header__burger-menu_click');
+});
+
+btnCalculate.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (form.checkValidity()) overlay.classList.add('overlay_show');
+  else return;
+});
+
+btnModal.addEventListener('click', () => {
+  overlay.classList.remove('overlay_show');
+});
+
+overlay.addEventListener('click', () => {
+  overlay.classList.remove('overlay_show');
 });
